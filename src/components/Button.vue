f00f2e3afa2ca4f42ac90c42804f5c6cdc9f7fcf
Adam Savander Jaworski <savander.pl@gmail.com>
2019-04-27 23:20:29 +0200

    ups, too much refactor. Rolling back few changes :D

---
+++ src/components/Button.vue
@@ -0,0 +1,32 @@
+<template>
+    <button class="btn" @click="$emit('click', $event)">
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+    export default {
+        name: "Button"
+    }
+</script>
+
+<style scoped>
+
+    .btn {
+        all: unset;
+        padding: 0.5rem 1.5rem 0.35rem;
+        line-height: 1em;
+        cursor: pointer;
+        color: aliceblue;
+        background-color: cornflowerblue;
+        border-radius: 3px;
+        border-bottom: 2px solid royalblue;
+        box-shadow: 0 5px 5px -5px darkslateblue;
+        transition: all 150ms ease-in-out;
+    }
+
+    .btn:hover {
+        transform: scale(1.05);
+        box-shadow: 0 5px 5px -3px darkslateblue;
+    }
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,102 @@
+<template>
+    <li
+        class="Todo-item"
+        :class="{isEven, isCompleted}"
+    >
+        <div
+            class="Todo-item-task"
+            :class="{isCompleted}"
+        >
+            {{task.task}}
+
+            <span class="Todo-item-date">
+                {{(new Date(task.createdAt)).toUTCString()}}
+            </span>
+        </div>
+
+        <Button @click="toggleTaskCompletion(task.id)">
+            <font-awesome-icon
+                :icon="isCompleted ? 'check-square' : 'square'"
+            />
+        </Button>
+    </li>
+</template>
+
+<script>
+    import Button from './Button';
+
+    export default {
+        name: "TodoItem",
+
+        components: {
+            Button
+        },
+
+        props: {
+            task: {
+                type: Object
+            },
+            isEven: {
+                type: Boolean,
+                required: true
+            }
+        },
+
+        methods: {
+            toggleTaskCompletion(id) {
+                this.$emit('toggleTask', id);
+            }
+        },
+
+        computed: {
+            isCompleted() {
+                return this.task.isCompleted;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .Todo-item {
+        all: unset;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 0.5rem 1.5rem;
+        background-color: aliceblue;
+        word-break: break-all;
+    }
+
+    .Todo-item:not(:last-child) {
+        margin-bottom: 0.25rem;
+    }
+
+    .Todo-item.even {
+        color: midnightblue;
+        background-color: lightsteelblue;
+    }
+
+    .Todo-item.isCompleted {
+        color: #444;
+        background-color: lightgray;
+    }
+
+    .Todo-item.isCompleted.even {
+        background-color: darkgray;
+    }
+
+    .Todo-item-task {
+        display: flex;
+        flex-direction: column;
+        margin-right: 0.5rem;
+        line-height: 1em;
+    }
+
+    .Todo-item-task.isCompleted {
+        text-decoration: line-through;
+    }
+
+    .Todo-item-date {
+        font-size: 0.75rem;
+    }
+</style>
